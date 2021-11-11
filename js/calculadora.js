af739775707b39dd2223834c9a10850961e4bcb7
Mariana Santana <s.santana.mariana@gmail.com>
2021-11-11 16:16:05 -0300

    Alteracao calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,22 +1,22 @@
 function inserir(num){
-    document.getElementById('resultado').innerHTML += num;
+    document.getElementById('resultado').value += num;
 }
 
 function limpar(){
-    document.getElementById('resultado').innerHTML = " ";
+    document.getElementById('resultado').value = " ";
 }
 
 function apagarCaracter(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+    var resultado = document.getElementById('resultado').value;
+    document.getElementById('resultado').value = resultado.substring(0, resultado.length -1);
 }
 
 function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;
+    var resultado = document.getElementById('resultado').value;
     if(resultado){
-        document.getElementById('resultado').innerHTML = eval(resultado);
+        document.getElementById('resultado').value = eval(resultado);
     }
     else{
-        document.getElementById('resultado').innerHTML = 0;
+        document.getElementById('resultado').value = "0";
     }    
 }
