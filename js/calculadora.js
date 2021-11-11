c119bb15417d8b32bf96c8147d225a04bb207d1f
Mariana Santana <s.santana.mariana@gmail.com>
2021-11-11 14:27:23 -0300

    Calculadora - Projeto

---
+++ js/calculadora.js
@@ -0,0 +1,22 @@
+function inserir(num){
+    document.getElementById('resultado').innerHTML += num;
+}
+
+function limpar(){
+    document.getElementById('resultado').innerHTML = " ";
+}
+
+function apagarCaracter(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else{
+        document.getElementById('resultado').innerHTML = 0;
+    }    
+}
